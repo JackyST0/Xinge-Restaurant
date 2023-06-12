9a484ae25ab43ad59f529100d57d1d4e7d967f7a
DawenzaiST <1700103031@qq.com>
2023-06-12 22:01:19 +0800

    更改了redis配置类相关配置

--- src/main/java/com/java/xinge/config/RedisConfig.java
+++ src/main/java/com/java/xinge/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -16,7 +17,7 @@ public class RedisConfig extends CachingConfigurerSupport {
 
         //默认的Key序列化器为：JdkSerializationRedisSerializer
         redisTemplate.setKeySerializer(new StringRedisSerializer()); // key序列化
-        //redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // value序列化
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // value序列化
 
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
